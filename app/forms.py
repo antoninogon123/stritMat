0b47a166e8b863e0d12d621a1f5f5e4c6d6deb54
Mr_Xzibit DA <gizatullin-2007@outlook.com>
2021-12-13 10:44:52 +0300

    first commit

---
+++ app/forms.py
@@ -0,0 +1,52 @@
+"""
+Definition of forms.
+"""
+
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import ugettext_lazy as _
+from.models import Comment 
+from.models import Blog
+
+class BootstrapAuthenticationForm(AuthenticationForm):
+    """Authentication form which uses boostrap CSS."""
+    username = forms.CharField(max_length=254,
+                               widget=forms.TextInput({
+                                   'class': 'form-control',
+                                   'placeholder': 'User name'}))
+    password = forms.CharField(label=_("Password"),
+                               widget=forms.PasswordInput({
+                                   'class': 'form-control',
+                                   'placeholder':'Password'}))
+
+class AnketaForm(forms.Form):
+    name = forms.CharField(label='Ваше имя', min_length=2, max_length=50)
+    city = forms.CharField(label='Ваш город', min_length=2, max_length=50)
+    email = forms.EmailField(label='Ваш e-mail', min_length=7)
+    gender = forms.ChoiceField(label="Ваш пол", 
+                               choices=[('1', 'Мужской'), ('2', 'Женский')],
+                               widget=forms.RadioSelect, initial=1)
+    pool = forms.ChoiceField(label='Тема отзыва',
+                               choices=(('1', 'Улучшение сайта'),
+                               ('2', 'Отзыв о магазине'),
+                               ('3', 'Улучшение каталога')), initial=1)
+    message = forms.CharField(label='Сообщение',
+                              widget=forms.Textarea(attrs={'rows':12, 'cols': 50}))
+    notice = forms.BooleanField(label='Получать новости сайта на e-mail?',
+                                required=False)
+
+class CommentForm (forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('text',)
+        labels = {'text': "Комментарий"}
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ('title', 'description', 'content', 'image',)
+        labels = {'title': "Заголовок",
+                  'description': "Краткое содержание",
+                  'content': "Полное содержание",
+                  'image': "Картинка"
+                  }
